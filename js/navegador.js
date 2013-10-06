70eaac3f107ddbffb72e908cd9fed02122c2e531
Alvarex David <deivid07.21@gmail.com>
2013-10-05 19:11:30 -0500

    JJ

--- js/navegador.js
+++ js/navegador.js
@@ -3,7 +3,7 @@ $(function(){
 	var idpas = 'none';
 
 	if(idpas=='none'){
-	navigater('pres');
+	navigater('log');
 	}
 		
 	$('#nav li a').bind('click',function(e){
@@ -14,13 +14,21 @@ $(function(){
 
 	function navigater(id){
 		if(id!=idpas){
-		$('#'+idpas+'-izquierda').animate({top:'100%'});
-		$('#'+idpas+'-derecha').animate({top:'-100%'});
-		$('#'+id+'-izquierda').animate({top:'45px'});
-		$('#'+id+'-derecha').animate({top:'45px'});
-		$('#'+idpas+'-izquierda').animate({top:'-100%'},0);
-		$('#'+idpas+'-derecha').animate({top:'100%'},0);
-		idpas=id;
+			$('#'+idpas+'-izquierda').animate({top:'100%'});
+			$('#'+idpas+'-derecha').animate({top:'-100%'});
+			$('#'+id+'-izquierda').animate({top:'45px'});
+			$('#'+id+'-derecha').animate({top:'45px'});
+			$('#'+idpas+'-izquierda').animate({top:'-100%'},0);
+			$('#'+idpas+'-derecha').animate({top:'100%'},0);
+
+
+			
+				if(id=='age'){
+					$('#'+id+'-mid').animate({height:'95%', top: '9%', opacity:'1'});
+				}else if(idpas=='age'){
+					$('#'+idpas+'-mid').animate({height:'0%', top: '45%', opacity:'0'});
+				}
+			idpas=id;
 		}
 	}
 });
